1415ae47e4db9d16463fe50cb1d74d049b99a05a
randallchacon <info@randallchacon.com>
2022-03-15 19:11:05 -0600

    primer commit

---
+++ Reservacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaUnitTesting
+{
+    public class Reservacion
+    {
+        public Usuario HechaPor { get; set; }
+
+        public bool CanceladaPor(Usuario usuario)
+        {
+            return (usuario.EsAdmin || HechaPor == usuario);
+        }
+    }
+
+    public class Usuario
+    {
+        public bool EsAdmin { get; set; }
+    }
+
+}
